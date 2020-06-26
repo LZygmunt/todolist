eddcc49b84f2260eca1630631c88972687152400
Lzygmunt <lzygmunt0@gmail.com>
2020-06-26 22:10:00 +0200

    move icons to separate components

---
+++ src/component/misc/Icons/LeftArrowIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LeftArrowIcon = () => (
+  <svg className="arrow icon" viewBox="0 0 100 100">
+    <polygon points="0,50 50,0 50,25 25,50 50,75 50,100" />
+  </svg>
+);
+
+export default LeftArrowIcon;
---
+++ src/component/misc/Icons/PlusIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlusIcon = () => (
+  <svg className="add-list icon" viewBox="-30 0 120 100">
+    <polygon points="30,0 60,0 60,30 90,30 90,60 60,60 60,90 30,90 30,60 0,60 0,30 30,30" />
+  </svg>
+);
+
+export default PlusIcon;
