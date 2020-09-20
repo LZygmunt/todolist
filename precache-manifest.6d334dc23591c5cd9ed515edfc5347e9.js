78c3ecc9d9a36cd2ca03760f56689b02333487ca
Lzygmunt <lzygmunt0@gmail.com>
2020-09-20 18:22:02 +0200

    Updates

---
+++ precache-manifest.6d334dc23591c5cd9ed515edfc5347e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccc4008044c370b4739c22965be937d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fbe75d63bcc406dd48fc",
+    "url": "/todolist/static/css/main.45bc2904.chunk.css"
+  },
+  {
+    "revision": "d46095c3baed9730845b",
+    "url": "/todolist/static/js/2.eaf2d836.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist/static/js/2.eaf2d836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe75d63bcc406dd48fc",
+    "url": "/todolist/static/js/main.77ab6a04.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
